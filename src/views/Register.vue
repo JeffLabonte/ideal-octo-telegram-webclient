11e2e4e0ec19849dbe1665ec107acb78c69296bd
Jeff Labonte <grimsleepless@protonmail.com>
2022-01-03 23:29:36 -0500

    Create Register view

---
+++ src/views/Register.vue
@@ -0,0 +1,12 @@
+<template>
+  <Register />
+</template>
+
+<script>
+import Register from "@/components/Register.vue";
+
+export default {
+  name: "Register",
+  components: { Register },
+};
+</script>
