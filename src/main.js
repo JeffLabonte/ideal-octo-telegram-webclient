0f13aebf232009ca8792c5533f736f226cd9f9c8
Jeff Labonte <grimsleepless@protonmail.com>
2022-01-09 23:19:09 -0500

    Add PWA bundle generated files

--- src/main.js
+++ src/main.js
@@ -2,5 +2,6 @@ import { createApp } from 'vue'
 import store from './store'
 import App from './App.vue'
 import router from './router'
+import './registerServiceWorker'
 
 createApp(App).use(router).use(store).mount('#app')
