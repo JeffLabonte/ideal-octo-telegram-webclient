0ea527a3f1076ea66cd62e8f6b0c6927fedb7a62
Jeff Labonte <grimsleepless@protonmail.com>
2022-01-13 21:51:33 -0500

    Remove about from project

--- src/router/index.js
+++ src/router/index.js
@@ -13,11 +13,6 @@ const routes = [
     name: 'Register',
     component: Register
   },
-  {
-    path: '/about',
-    name: 'About',
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  },
 ]
 
 const router = createRouter({
