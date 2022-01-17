daacec8c4857f628c43c468b853cb2004b6896be
Jeff Labonte <grimsleepless@protonmail.com>
2022-01-17 00:28:56 -0500

    Refactor the registration form

--- src/components/Register.vue
+++ src/components/Register.vue
@@ -1,41 +1,58 @@
 <template>
-  <form
-    id="register"
-    @submit="checkForm"
-    action="http://backend:80/api/atuh/registration"
-    method="post"
-  >
-    <p v-if="errors.length">
-      <b>Please correct the following error(s):</b>
+  <form id="register" @submit="registerAccount">
+    <div v-if="errors.length">
+      <p>Please fix the following errors:</p>
       <ul>
-        {{ errors  }}
+        <li v-for="(index, error) in errors" :key="index">{{ error }}</li>
       </ul>
-    </p>
+    </div>
     <div id="email-box">
       <p>
         <label for="email">Please enter your email:</label>
-        <input id="email" v-model="input.email" type="email" name="email" placeholder="you@company.com" />
+        <input
+          id="email"
+          v-model="input.email"
+          type="email"
+          name="email"
+          placeholder="you@company.com"
+        />
       </p>
 
       <p>
         <label for="email-confirmation">Please confirm your email:</label>
-        <input id="email-confirmation" v-model="input.emailConfirmation" type="email" name="email-confirmation" placeholder="you@acompany.com" />
+        <input
+          id="email-confirmation"
+          v-model="input.emailConfirmation"
+          type="email"
+          name="email-confirmation"
+          placeholder="you@acompany.com"
+        />
       </p>
     </div>
     <div>
       <p>
         <label for="password">Please enter your pasword:</label>
-        <input id="password" v-model="input.password" type="password" name="password" placeholder="Th1s!M1gHt!@Be%4@G00d$1dea:)"/>
+        <input
+          id="password"
+          v-model="input.password"
+          type="password"
+          name="password"
+          placeholder="Th1s!M1gHt!@Be%4@G00d$1dea:)"
+        />
       </p>
       <p>
         <label for="password-confirmation">Confirm it please, just in case:</label>
-        <input id="password-confirmation" v-model="input.passwordConfirmation" type="password" placeholder="0r!Th1s(:"/>
+        <input
+          id="password-confirmation"
+          v-model="input.passwordConfirmation"
+          type="password"
+          placeholder="0r!Th1s(:"
+        />
       </p>
     </div>
-    <p>
-      <label for=""></label>
-      <input />
-    </p>
+    <div>
+      <input id="submi" name type="submit" value="Register" />
+    </div>
   </form>
 </template>
 
@@ -53,6 +70,11 @@ export default {
       },
       error: [],
     };
+  },
+  methods: {
+    registerAccount() {
+      this.errors.append("This is a test");
+    },
   },
 };
 </script>
