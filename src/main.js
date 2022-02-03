9a3af5ffc4ed7d395f16f7343a9564d8b10a424a
Jeff Labonte <grimsleepless@protonmail.com>
2022-02-03 00:40:59 -0500

    Change vanilla VueJS for NuxtJS

--- src/main.js
+++
@@ -1,7 +0,0 @@
-import { createApp } from 'vue'
-import store from './store'
-import App from './App.vue'
-import router from './router'
-import './registerServiceWorker'
-
-createApp(App).use(router).use(store).mount('#app')
