d812e346c87030d67f08857e4863ec6ff33e1528
Jeff Labonte <grimsleepless@protonmail.com>
2022-02-08 00:36:13 -0500

    Create the first base to login

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,17 @@
 <template>
-  <Tutorial />
+  <main class="form-signin">
+    <img src alt width="72" height="57" />
+    <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
+    <div class="form-floating">
+      <input type="email" class="form-control" id="floatingInput" placeholder="name@examplecom" />
+      <label for="floatingInput">Email Address</label>
+    </div>
+    <div class="form-floating">
+      <input type="password" class="form-control" id="floatingPassword" placeholder="Password" />
+      <label for="floatingPassword">Password</label>
+    </div>
+    <button clas="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
+  </main>
 </template>
 
 <script>
