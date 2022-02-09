26e3fa49b750d3dd20a276c9246cd43da9a6faf4
Jeff Labonte <grimsleepless@protonmail.com>
2022-02-09 00:37:06 -0500

    Update the login page to have access to the register page

--- pages/index.vue
+++ pages/index.vue
@@ -3,14 +3,25 @@
     <img src alt width="72" height="57" />
     <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
     <div class="form-floating">
-      <input type="email" class="form-control" id="floatingInput" placeholder="name@examplecom" />
+      <input id="floatingInput" class="form-control" type="email" placeholder="name@examplecom" />
       <label for="floatingInput">Email Address</label>
     </div>
     <div class="form-floating">
-      <input type="password" class="form-control" id="floatingPassword" placeholder="Password" />
+      <input id="floatingPassword" class="form-control" type="password" placeholder="Password" />
       <label for="floatingPassword">Password</label>
     </div>
-    <button clas="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
+    <div class="container">
+      <div class="row">
+        <div class="column">
+          <button class="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
+        </div>
+        <div class="column">
+          <button class="w-100 btn btn-lg">
+            <NuxtLink to="/register">Register new account</NuxtLink>
+          </button>
+        </div>
+      </div>
+    </div>
   </main>
 </template>
 
