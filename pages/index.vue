245fed301ec0542e0e5f3d7c79da6d92f8405b0f
Jeff Labonte <grimsleepless@protonmail.com>
2022-03-06 01:25:35 -0500

    Add model and function to login

--- pages/index.vue
+++ pages/index.vue
@@ -3,17 +3,29 @@
     <img src alt width="72" height="57" />
     <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
     <div class="form-floating">
-      <input id="floatingInput" class="form-control" type="email" placeholder="name@examplecom" />
+      <input
+        id="floatingInput"
+        v-model="email"
+        class="form-control"
+        type="email"
+        placeholder="name@examplecom"
+      />
       <label for="floatingInput">Email Address</label>
     </div>
     <div class="form-floating">
-      <input id="floatingPassword" class="form-control" type="password" placeholder="Password" />
+      <input
+        id="floatingPassword"
+        v-model="password"
+        class="form-control"
+        type="password"
+        placeholder="Password"
+      />
       <label for="floatingPassword">Password</label>
     </div>
     <div class="container">
       <div class="row">
         <div class="column">
-          <button class="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
+          <button @click="login" class="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
         </div>
         <div class="column">
           <button class="w-100 btn btn-lg">
@@ -28,5 +40,20 @@
 <script>
 export default {
   name: 'IndexPage',
+  data: () => {
+    return {
+      email: '',
+      password: '',
+      loginUrl: 'http://192.168.68.117/api/auth/login',
+    };
+  },
+  methods: {
+    login() {
+      this.$store.dispatch('login', {
+        username: this.username,
+        password: this.password,
+      });
+    },
+  },
 };
 </script>
