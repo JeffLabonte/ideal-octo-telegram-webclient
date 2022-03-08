c696f749e001d7a8c0985bf62834d573ddeaee81
Jeff Labonte <grimsleepless@protonmail.com>
2022-03-07 22:17:58 -0500

    Complete login happy path

--- store/index.js
+++ store/index.js
@@ -1,21 +1,30 @@
 export const state = () => ({
   apiKey: null,
-  loginUrl: 'http://192.168.68.117/api/auth/login',
+  loginUrl: 'http://192.168.68.117/api/auth/login/',
 });
 
-export const getters = {};
+export const getters = {
+  getApiKey(state) {
+    return state.apiKey;
+  }
+};
 export const mutations = {
-  setApiKey(state, {apiKey}) {
+  setApiKey(state, apiKey) {
     state.apiKey = apiKey;
   }
 };
+
 export const actions = {
   login(context, payload) {
     this.$axios.post(
-      context.loginUrl,
+      context.state.loginUrl,
       payload,
     ).then((response) => {
-      console.log(response);
+      const apiKey = response.data.key;
+      context.commit("setApiKey", apiKey);
+      this.$router.push("/home")
+    }).catch((error) => {
+      console.log(error);
     });
   }
 };
